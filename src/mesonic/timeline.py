37d185adf429432a56e848ae9984b585d454dd80
Dennis Reinsch <dreinsch@techfak.uni-bielefeld.de>
2023-07-24 12:34:15 +0200

    Add new plot_new method to Timeline and preparation methods

--- src/mesonic/timeline.py
+++ src/mesonic/timeline.py
@@ -1,12 +1,25 @@
 import copy
 import math
+from collections import defaultdict
 from threading import RLock
 from time import time as get_timestamp
-from typing import Callable, Dict, List, Optional, Tuple
+from typing import TYPE_CHECKING, Callable, Dict, List, Optional, Tuple
 
 import attr
+import numpy as np
+import pyamapping as pam
+from attrs import define
 
 from mesonic.events import Event, SynthEvent, SynthEventType
+
+if TYPE_CHECKING:
+    from mesonic.synth import Synth
+
+
+@define(kw_only=True, slots=True, repr=True)
+class SynthEventPseudoStop(Event):
+    synth: "Synth" = (attr.field(repr=False),)
+    etype: SynthEventType = SynthEventType.STOP
 
 
 @attr.s(slots=True, repr=True)
@@ -334,6 +347,274 @@ class Timeline:
         if value < 0:
             raise ValueError("end_time_offset must larger or equal 0")
         self._end_time_offset = value
+
+    def _split_synth_events(self) -> Dict["Synth", Dict["SynthEventType", List[Event]]]:
+        splitted_synth_events = {}
+        for time, events in self.to_dict().items():
+            for event in events:
+                if isinstance(event, SynthEvent):
+                    if event.synth not in splitted_synth_events:
+                        splitted_synth_events[event.synth] = {
+                            SynthEventType.START: [],
+                            SynthEventType.STOP: [],
+                            SynthEventType.SET: [],
+                        }
+                    splitted_synth_events[event.synth][event.etype].append(
+                        (time, event)
+                    )
+        return splitted_synth_events
+
+    @staticmethod
+    def _create_stop_event(
+        event_info, duration_key="dur", default_duration=0.1
+    ) -> Tuple[float, "SynthEventPseudoStop"]:
+        time, start_event = event_info
+        duration = start_event.data.get(duration_key, default_duration)
+        return time + duration, SynthEventPseudoStop(
+            track=start_event.track, info=start_event.info, synth=start_event.synth
+        )
+
+    def _check_consistency(self, duration_key="dur"):
+        splitted_synth_events = self._split_synth_events()
+        for synth, events in splitted_synth_events.items():
+            if synth.mutable:
+                start_stop_diff = len(events[SynthEventType.START]) - len(
+                    events[SynthEventType.STOP]
+                )
+                if start_stop_diff == 0:
+                    # for each start there is a stop
+                    continue
+                elif start_stop_diff == len(events[SynthEventType.START]):
+                    # not stops were made
+                    events[SynthEventType.STOP] = list(
+                        map(self._create_stop_event, events[SynthEventType.START])
+                    )
+                elif start_stop_diff > 0:
+                    # search missing stops and generate pseudo stops
+                    idx = 1
+                    nr_stops_generated = 0
+                    while (
+                        idx < len(events[SynthEventType.START])
+                        or nr_stops_generated == start_stop_diff
+                    ):
+                        start_1 = events[SynthEventType.START][idx - 1]
+                        start_2 = events[SynthEventType.START][idx]
+                        stop = events[SynthEventType.STOP][idx - 1]
+                        assert start_1[0] < start_2[0], "starts overlap"
+                        assert start_1[0] < stop[0], "stop without prior start"
+                        if start_2[0] < stop[0]:
+                            # stop does stop start_2 -> start_1 is not stopped
+                            start_time, start_event = start_1
+                            events[SynthEventType.STOP].insert(
+                                idx - 1, self._create_stop_event(start_event)
+                            )
+                            nr_stops_generated += 1
+                        idx += 1
+                    if nr_stops_generated < start_stop_diff:
+                        # last start is unmatched.
+                        events[SynthEventType.STOP].append(
+                            self._create_stop_event(events[SynthEventType.START][-1])
+                        )
+                else:
+                    raise RuntimeError(f"Timeline has more Stop Events for {synth}")
+            else:  # synth immutable
+                assert len(events[SynthEventType.STOP]) == 0
+                assert len(events[SynthEventType.SET]) == 0
+                events[SynthEventType.STOP] = list(
+                    map(self._create_stop_event, events[SynthEventType.START])
+                )
+        return splitted_synth_events
+
+    def _create_segments(self, offset_key="freq", width_key="amp"):
+        splitted_synth_events = self._check_consistency()
+        # for quiver X, U, Y (V=0), arrowwidths
+        segments = {"begins": [], "ends": [], "offsets": [], "widths": []}
+        # for scatter tuples (x,y)
+        markers = {"start": [], "stop": [], "set": []}
+        synth_plot_info = {
+            synth: {
+                "events": events,
+                "segments": copy.deepcopy(segments),
+                "markers": copy.deepcopy(markers),
+            }
+            for synth, events in splitted_synth_events.items()
+        }
+        for synth, events in splitted_synth_events.items():
+            begins = synth_plot_info[synth]["segments"]["begins"]
+            ends = synth_plot_info[synth]["segments"]["ends"]
+            offsets = synth_plot_info[synth]["segments"]["offsets"]
+            widths = synth_plot_info[synth]["segments"]["widths"]
+
+            for time, event in events[SynthEventType.START]:
+                begins.append(time)
+                offsets.append((event.data[offset_key]))
+                widths.append((event.data[width_key]))
+            ends.extend([t[0] for t in events[SynthEventType.STOP]])
+            if events[
+                SynthEventType.SET
+            ]:  # if there are SET events we need to split segments
+                # we need to cluster the set events as they are only singular
+                combined_set_events = defaultdict(list)
+                for set_time, set_event in events[SynthEventType.SET]:
+                    combined_set_events[set_time].append(set_event)
+                insert_idx = 0
+                # begins = begins
+                for set_time, set_events in combined_set_events.items():
+                    # set search index back by one
+                    while insert_idx < len(begins) and begins[insert_idx] < set_time:
+                        insert_idx += 1
+                    # get properties for the event data
+                    found_offset = False
+                    found_width = False
+                    for set_event in set_events:
+                        if set_event.data["name"] == offset_key:
+                            offset = set_event.data["new_value"]
+                            offsets.insert(insert_idx, offset)
+                            found_offset = True
+                        if set_event.data["name"] == width_key:
+                            width = set_event.data["new_value"]
+                            widths.insert(insert_idx, width)
+                            found_width = True
+                    if found_offset or found_width:
+                        # start_idx is now the index of the segment begin that our set interrupts
+                        begins.insert(insert_idx, set_time)
+                        # insert the new segment end before the old segment end
+                        ends.insert(insert_idx - 1, set_time)
+                        # if a line property was not found use the value from the prior segment
+                        if not found_offset:
+                            offset = offsets[insert_idx - 1]
+                            offsets.insert(insert_idx, offset)
+                        if not found_width:
+                            width = widths[insert_idx - 1]
+                            widths.insert(insert_idx, width)
+
+            assert len(begins) == len(ends)
+            assert len(begins) == len(offsets)
+            assert len(begins) == len(widths)
+        return synth_plot_info
+
+    def plot_new(
+        self, offset="freq", width="amp", duration_key="dur", default_duration=0.1
+    ):
+        """Plot the Timeline.
+
+        Parameters
+        ----------
+        parameter_key : str, optional
+            The Parameter name for the y-axis offset, by default "freq"
+            This is used to get the bounds of the parameter and set the
+            y-axis offset of the synth accordingly.
+        duration_key : str, optional
+            The Parameter name for the Synth duration, by default "dur"
+            This is used to extract the duration from the Events.
+        default_duration : float, optional
+            The fallback value if no duration can be found, by default 0.1
+
+        Raises
+        ------
+        ImportError
+            If matplotlib cannot be imported.
+        """
+        try:
+            import matplotlib.pyplot as plt
+        except ImportError as err:
+            raise ImportError(
+                "plotting the Timeline is only possible when matplotlib is installed."
+            ) from err
+        # do not plot an empty Timeline
+        if self.is_empty():
+            return
+
+        # Events offer
+        # track, info
+        # SynthEvents offer
+        # synth, etype, data
+        # dict with synth as key and a inner dict
+        # {
+        #   for each synth synth
+        #   "warp": "lin"|"log"|"exp",
+        #   "color":
+        #   "y_offset": 0 (depending on synth & track)
+        #
+        #   events
+        #       "starts": [(x,y,s)],
+        #       "stops": [(x,y,s)], (virtuell)
+        #       "sets": [(x,y,s)],
+        #
+        #   for each segment
+        #   segments
+        #       starts
+        #       stops
+        #       "offsets": [], (depending on 1st value)
+        #       "widths": [],  (depending on 2nd value)
+        #
+        # }
+        # so one can use quiver for the segments
+
+        # get segments
+        synth_plot_info = self._create_segments(offset_key=offset, width_key=width)
+
+        # synth specifics are added later
+        # "warp": None,s
+        # "bounds": None,
+        # "color": None,
+        # "y_offset": 0,
+
+        fig = plt.figure()
+        axes = fig.add_subplot(1, 1, 1)
+        from matplotlib.collections import LineCollection
+
+        x_min, x_max = 0, 0
+        y_min, y_max = 0, 0
+
+        for synth in synth_plot_info.keys():
+            segments = synth_plot_info[synth]["segments"]
+            begins = np.array(segments["begins"])
+            ends = np.array(segments["ends"])
+            offsets = np.array(segments["offsets"])
+            widths = np.array(segments["widths"])
+
+            # TODO implement warps as two or one parameterized fun
+            def warp_offset(value, scale=None):
+                return value
+
+            def warp_width(value, scale=None, width_min=2, width_max=4):
+                if widths.min() == widths.max():
+                    return (width_max + width_min) / 2
+                ret = pam.linlin(
+                    value, widths.min(), widths.max(), width_min, width_max
+                )
+                print(value, ret)
+                return ret
+
+            offsets = warp_offset(offsets)
+            widths = warp_width(widths)
+            lines = np.array(
+                list(
+                    zip(
+                        list(zip(begins, offsets)),
+                        list(zip(ends, offsets)),
+                    )
+                )
+            )
+            lc = LineCollection(
+                lines,
+                linewidth=widths,
+            )
+            axes.add_collection(lc)
+
+            x_min = min(begins.min(), x_min)
+            x_max = max(ends.max(), x_max)
+            y_min = min(offsets.min(), y_min)
+            y_max = max(offsets.max(), y_max)
+
+        x_lim = (x_max - x_min) * 0.1
+        axes.set_xlim(x_min - x_lim, x_max + x_lim)
+        y_lim = (y_max - y_min) * 0.1
+        axes.set_ylim(y_min - y_lim, y_max + y_lim)
+        axes.grid()
+
+    # TODO Idea: convention to order the Synth Params by most probalbe usage
 
     def plot(self, parameter_key="freq", duration_key="dur", default_duration=0.1):
         """Plot the Timeline.
